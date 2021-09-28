8611be406855607e0afcf95c0abfdf578e6e1eec
Tkachev Nikita Stanislavovich <tkachiov.2000@gmail.com>
2021-09-28 17:56:37 +0300

    work

--- src/script/index.js
+++ src/script/index.js
@@ -2,9 +2,35 @@ import newGame from './newGame';
 import a_sleep from './a_sleep';
 import a_tiktok from './a_tiktok';
 import b_wakeUp from './b_wakeUp';
+import b_findBar from './b_findBar';
+import b_home from './b_home';
+import b_town from './b_town';
+import c_doYouRemember from './c_doYouRemember';
+import c_plead from './c_plead';
+import c_shipper from './c_shipper';
+import d_checkBar from './d_checkBar';
+import d_checkDiner from './d_checkDiner';
+import d_checkHouses from './d_checkHouses';
+import d_getFood from './d_getFood';
+import e_date from './e_date';
+import gameOver from './gameOver';
+import gameOverEndless from './gameOverEndless';
 export default {
   newGame,
   a_sleep,
   a_tiktok,
   b_wakeUp,
+  b_findBar,
+  b_home,
+  b_town,
+  c_doYouRemember,
+  c_plead,
+  c_shipper,
+  d_checkBar,
+  d_checkDiner,
+  d_checkHouses,
+  d_getFood,
+  e_date,
+  gameOver,
+  gameOverEndless,
 };
